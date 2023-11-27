8d3147a31f78b84328e841abe8b13d8369f760f0
jangh <jangh@vmware.com>
2023-11-27 15:59:48 +0900

    update

--- deployment.sh
+++ deployment.sh
@@ -2,4 +2,9 @@
 
 pip install --no-cache-dir fastapi uvicorn aiohttp asyncio requests psycopg psycopg-binary
 
-cd /opt/cicd/src; python
+if [ ! -f "/lib/systemd/system/fapi.service" ]; then
+	sudo -i cp /opt/cicd/src/files/fapi.service /lib/systemd/system/fapi.service
+	systemctl enable fapi.service
+fi
+
+systemctl restart fapi.service
