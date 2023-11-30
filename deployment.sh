e4b8b3b615c8869273294e36a4cf96cd3eadff20
jangh <jangh@vmware.com>
2023-11-30 22:24:01 +0900

    update

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+source .cicd/files/ini_parser.sh
+process_ini_file "deployment.ini" > /dev/null
 
+# Load Variables from INI ###################################################
+
+# Deployment Script Here ####################################################
 pip install --no-cache-dir fastapi uvicorn aiohttp asyncio requests psycopg psycopg-binary 2>&1 > /dev/null
 
 if [ ! -f "/lib/systemd/system/fapi.service" ]; then
